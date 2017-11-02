c31a181a71c365ac80b4c3f734aed9756d731c8a
Dean Glueckler <glueckler@gmail.com>
2017-11-02 08:23:17 -0700

    Added messaging ability

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ class App extends Component {
       currentUser: props.data.currentUser,
       loaded: false
     }
+    this.onNewPost = ({content, username}) => {
+      const messages = this.state.messages.concat({content, username})
+      this.setState({
+        messages
+      })
+
+    }
   }
 
   componentDidMount() {
@@ -23,11 +30,8 @@ class App extends Component {
     console.log("componentDidMount <App />");
     setTimeout(() => {
       console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
       const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
       const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
       this.setState({messages: messages})
     }, 3000);
 
@@ -46,7 +50,7 @@ class App extends Component {
                 Anonymous1 changed their name to nomnom.
               </div>
             </main>
-            <Chatbar/>
+            <Chatbar onNewPost={ this.onNewPost }/>
           </section>
         )
     } else {
@@ -57,23 +61,3 @@ class App extends Component {
 }
 export default App
 
-/*
-  1.   Copy html layout into App.jsx.
-  2.   Convert class and other reserved names to React versions.
-  3.   Break html into separate components to create a hierarchy.
-  4.   Add state to App.jsx to manage the current posts.
-  5.   Pass the posts to the child components as a prop.
-  6.   In the posts series component map over the posts array.
-  7.   In the posts component use the prop to replace the static content.
-  8.   In the header setup the content state.
-  9.   Pass the current and maximum character count to the status bar as props.
-  10.  In the status bar render the character counter, changing the style when max is reached.
-  11.  Setup an event handler to trigger an update to the content on input.
-  12.  Pass a function to the header that it can call when a new post is created.
-  13.  Setup an event handler to use the function, passed as a prop.
-  14.  In the header create the error state and pass it as a prop to the status.
-  15.  Show the error text if it is not empty.
-  16.  Switch from hard coded posts to json that uses a fetch (ajax) call.
-  17.  Save the new post to the database in the App.jsx post handler function.
-  18.  Toggle the visibility of the compose window.
-*/
--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,25 +1,49 @@
 import React, {Component} from 'react'
 
+// do you always extend component?
 class Chatbar extends Component {
 
   constructor(props) {
     super(props)
     this.state = {
-      content: ''
+      content: '',
+      username: ''
     }
     this.onContent = (event) => {
-      console.log(event.target.innerText)
       this.setState({
+        //how to find the event information
         content: event.target.value
       })
+    }
+    this.onUserName = (event) => {
+      this.setState({
+        username: event.target.value
+      })
+    }
+  }
+
+
+  // define methods here.. or inside the constructor?
+  postIfEnterKey(event) {
+    if (event.key === "Enter") {
+
+      const {content, username} = this.state;
+      const newMessage = {content, username};
+
+      // const newMessage = this.state;
+
+      this.props.onNewPost(newMessage)
+      //dont do this
+      this.state.content = ''
+      this.state.username = ''
     }
   }
 
   render() {
     return(
         <footer className="chatbar">
-          <input className="chatbar-username" placeholder="Your Name (Optional)" />
-          <input onChange={ this.onContent } value={ this.state.content } className="chatbar-message" placeholder="Type a message and hit ENTER" />
+          <input onChange={ this.onUserName } value={ this.state.username } className="chatbar-username" placeholder="Your Name (Optional)" />
+          <input onChange={ this.onContent } value={ this.state.content } onKeyPress={ this.postIfEnterKey.bind(this) } className="chatbar-message" placeholder="Type a message and hit ENTER" />
         </footer>
       )
   }
